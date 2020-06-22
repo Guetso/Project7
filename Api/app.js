648371d1ab46b83dcfa0895b6a2b93a596ef465c
Guetso <legalloisprovensal@gmail.com>
2020-06-22 11:20:52 +0200

    init api

---
+++ Api/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !')
+  next()
+})
+
+app.use((req, res, next) => {
+  res.status(201)
+  next()
+})
+
+app.use((req, res, next) => {
+  res.json({ message: 'Ok code requête !' })
+})
+
+module.exports = app
