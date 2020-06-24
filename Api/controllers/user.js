e24e43751655832842e90a0d23c64ea192986893
Guetso <legalloisprovensal@gmail.com>
2020-06-24 14:35:26 +0200

    add bcrypt to signup query

--- Api/controllers/user.js
+++ Api/controllers/user.js
@@ -1,12 +1,23 @@
 const conn = require('../mysqlConfig')
+const bcrypt = require('bcrypt')
 
 exports.signup = (req, res, next) => {
-  const user = req.body.user
-
-  conn.query('INSERT INTO users SET ?', user, function (error, results, fields) {
-    if (error) { // Si erreur de la requête
-      console.log(error) // La console du serveur affiche l'erreur
-      return res.status(400).json(error.sqlMessage)} // Et retourne uniquement le message de l'erreur au front
-    return res.status(201).json({message: 'Bienvenue ! Vous pouvez maintenant vous connecter.'})
+  let user = req.body.user
+  bcrypt.hash(user.password, 10).then((hash) => {
+    user.password = hash
+    conn.query('INSERT INTO users SET ?', user, function (
+      error,
+      results,
+      fields
+    ) {
+      if (error) {
+        // Si erreur de la requête
+        console.log(error) // La console du serveur affiche l'erreur
+        return res.status(400).json(error.sqlMessage)
+      } // Et retourne uniquement le message de l'erreur au front
+      return res.status(201).json({
+        message: 'Bienvenue ! Vous pouvez maintenant vous connecter.',
+      })
+    })
   })
 }
