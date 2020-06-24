49ceda29b515b68b5cede1a9bf0a084027f15e56
Guetso <legalloisprovensal@gmail.com>
2020-06-24 16:59:28 +0200

    update style signup

--- Front/groupomania-front/src/App.vue
+++ Front/groupomania-front/src/App.vue
@@ -24,7 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  border: solid 2px red;
   padding: 5px;
   height: 100vh;
 }
--- Front/groupomania-front/src/views/Signup.vue
+++ Front/groupomania-front/src/views/Signup.vue
@@ -4,20 +4,55 @@
 
     <form class="signupForm" @submit="submitForm" v-if="show">
       <label for="email">E-mail:</label>
-      <input type="text" name="email" v-model="formData.user.email" placeholder="Email" />
+      <input
+        type="email"
+        name="email"
+        v-model="formData.user.email"
+        placeholder="Email"
+        required
+        maxlength="50"
+      />
 
       <label for="userName">Nom d'utilisateur:</label>
-      <input type="text" name="userName" v-model="formData.user.userName" placeholder="User" />
+      <input
+        type="text"
+        name="userName"
+        v-model="formData.user.userName"
+        placeholder="User"
+        required
+        maxlength="50"
+      />
 
       <label for="password">Mot de passe:</label>
-      <input type="text" name="password" v-model="formData.user.password" placeholder="Password" />
+      <input
+        type="password"
+        name="password"
+        v-model="formData.user.password"
+        placeholder="Password"
+        pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$"
+      />
 
       <button type="submit">Valider</button>
     </form>
 
-    <aside class="messageService" v-if="feedbacks.length">
+    <div class="messageService" v-if="feedbacks.length">
       <ul>
-        <li v-for="feedback in feedbacks" :key="feedback.message">{{ feedback.message }}</li>
+        <p v-for="feedback in feedbacks" :key="feedback.message">
+          {{ feedback.message }}
+          <router-link :to="{ name: 'Login' }">Login</router-link>
+        </p>
+      </ul>
+    </div>
+
+    <aside class="passwordConditions" v-if="show">
+      Pour des raisons de sécurité votre mot de passe doit contenir au moins:
+      <br />
+      <ul>
+        <li>entre 8 à 15 caractères</li>
+        <li>une lettre minuscule</li>
+        <li>une lettre majuscule</li>
+        <li>un chiffre</li>
+        <li>un de ces caractères spéciaux: $ @ % * + - _ !</li>
       </ul>
     </aside>
   </div>
@@ -57,23 +92,31 @@ export default {
     };
   },
   methods: {
-    submitForm: function(e) { // lorsque le bouton de validation du formulaire est cliqué :
+    submitForm: function(e) {
+      // lorsque le bouton de validation du formulaire est cliqué :
       e.preventDefault(); // On bloque la redirection
-      post("http://localhost:3000/api/auth/signup", this.formData) // On appelle la fonction POST (requête XHR), Cette fonction renvoie une promesse 
-        .then(response => { // En cas de réussite de la requête (resolve), la fonction nous renvoie la réponse du serveur
+      post("http://localhost:3000/api/auth/signup", this.formData) // On appelle la fonction POST (requête XHR), Cette fonction renvoie une promesse
+        .then(response => {
+          // En cas de réussite de la requête (resolve), la fonction nous renvoie la réponse du serveur
           this.show = false; // On cache le formulaire
-          this.feedbacks.push({ // On pousse le message de la réponse dans le tableau feedbacks
+          this.feedbacks.push({
+            // On pousse le message de la réponse dans le tableau feedbacks
             message: response.message // le feedback s'affiche car il n'est plus vide
           });
         })
-        .catch(error => { // En cas d'échec de la requête
+        .catch(error => {
+          // En cas d'échec de la requête
           this.show = false; //  On cache le formulaire
           console.error(error.response); // La console renvoie la réponse de l'erreur
-          if (error.response.indexOf('users.email_UNIQUE')!== -1) { // Dans le cas d'un email déjà utilisé
-            this.feedbacks.push({ message: 'Cet email est déjà utilisé !' });
+          if (error.response.indexOf("users.email_UNIQUE") !== -1) {
+            // Dans le cas d'un email déjà utilisé
+            this.feedbacks.push({ message: "Cet email est déjà utilisé !" });
           }
-          if (error.response.indexOf('users.username_UNIQUE')!== -1) { // Dans le cas d'un username déjà utiisé
-            this.feedbacks.push({message: 'Ce nom d\'utilisateur est déjà utilisé !'})
+          if (error.response.indexOf("users.username_UNIQUE") !== -1) {
+            // Dans le cas d'un username déjà utiisé
+            this.feedbacks.push({
+              message: "Ce nom d'utilisateur est déjà utilisé !"
+            });
           }
         });
     }
