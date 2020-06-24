76b5c120857e8c0926644990be27abe9bb63d010
Guetso <legalloisprovensal@gmail.com>
2020-06-24 11:27:03 +0200

    Gestion des erreurs signup

--- Api/controllers/user.js
+++ Api/controllers/user.js
@@ -4,7 +4,9 @@ exports.signup = (req, res, next) => {
   const user = req.body.user
 
   conn.query('INSERT INTO users SET ?', user, function (error, results, fields) {
-    if (error) throw error
-    return res.status(201).json({message: 'New user created !'})
+    if (error) { // Si erreur de la requête
+      console.log(error) // La console du serveur affiche l'erreur
+      return res.status(400).json(error.sqlMessage)} // Et retourne uniquement le message de l'erreur au front
+    return res.status(201).json({message: 'Bienvenue ! Vous pouvez maintenant vous connecter.'})
   })
 }
--- Front/groupomania-front/src/views/Signup.vue
+++ Front/groupomania-front/src/views/Signup.vue
@@ -2,7 +2,7 @@
   <div id="Signup">
     <h2>{{ title }}</h2>
 
-    <form class="signupForm" @submit="submitForm" v-if="!show">
+    <form class="signupForm" @submit="submitForm" v-if="show">
       <label for="email">E-mail:</label>
       <input type="text" name="email" v-model="formData.user.email" placeholder="Email" />
 
@@ -16,13 +16,10 @@
     </form>
 
     <aside class="messageService" v-if="feedbacks.length">
-      
       <ul>
-        <li v-for="feedback in feedbacks" :key="feedback.message"> {{ feedback.message }}</li>
+        <li v-for="feedback in feedbacks" :key="feedback.message">{{ feedback.message }}</li>
       </ul>
-    
     </aside>
-    
   </div>
 </template>
 
@@ -55,19 +52,30 @@ export default {
       formData: {
         user: { email: null, userName: null, password: null, isAdmin: 0 }
       },
-      feedbacks: [],
-      show: false
+      feedbacks: [], // Nous permets d'afficher les informations suite à la soumission du formulaire, contient des objets au format {message : 'contenu du message'},il n'apparait que si non vide
+      show: true // Le formulaire doit il être affiché
     };
   },
   methods: {
-    submitForm: function(e) {
-      e.preventDefault();
-      post("http://localhost:3000/api/auth/signup", this.formData)
-        .then(
-          (this.show = true),
-          this.feedbacks.push({message:"Bienvenue ! Vous pouvez vous connecter"})
-        )
-        .catch(error => console.log(error));
+    submitForm: function(e) { // lorsque le bouton de validation du formulaire est cliqué :
+      e.preventDefault(); // On bloque la redirection
+      post("http://localhost:3000/api/auth/signup", this.formData) // On appelle la fonction POST (requête XHR), Cette fonction renvoie une promesse 
+        .then(response => { // En cas de réussite de la requête (resolve), la fonction nous renvoie la réponse du serveur
+          this.show = false; // On cache le formulaire
+          this.feedbacks.push({ // On pousse le message de la réponse dans le tableau feedbacks
+            message: response.message // le feedback s'affiche car il n'est plus vide
+          });
+        })
+        .catch(error => { // En cas d'échec de la requête
+          this.show = false; //  On cache le formulaire
+          console.error(error.response); // La console renvoie la réponse de l'erreur
+          if (error.response.indexOf('users.email_UNIQUE')!== -1) { // Dans le cas d'un email déjà utilisé
+            this.feedbacks.push({ message: 'Cet email est déjà utilisé !' });
+          }
+          if (error.response.indexOf('users.username_UNIQUE')!== -1) { // Dans le cas d'un username déjà utiisé
+            this.feedbacks.push({message: 'Ce nom d\'utilisateur est déjà utilisé !'})
+          }
+        });
     }
   }
 };
