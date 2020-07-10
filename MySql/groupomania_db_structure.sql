8c8ca6bf1df6ef835361effc10abb9dd11757176
Guetso <legalloisprovensal@gmail.com>
2020-07-10 11:51:27 +0200

    update Database

--- MySql/groupomania_db_structure.sql
+++ MySql/groupomania_db_structure.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `groupomania` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `groupomania`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: groupomania
@@ -16,6 +14,49 @@ USE `groupomania`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `likes` (
+  `idUSERS` int NOT NULL,
+  `idMESSAGES` int NOT NULL,
+  PRIMARY KEY (`idUSERS`,`idMESSAGES`),
+  KEY `fk_LIKES_MESSAGES_idx` (`idMESSAGES`),
+  KEY `fk_LIKES_USERS_idx` (`idUSERS`),
+  CONSTRAINT `fk_LIKES_MESSAGES` FOREIGN KEY (`idMESSAGES`) REFERENCES `messages` (`idMESSAGES`) ON DELETE CASCADE,
+  CONSTRAINT `fk_LIKES_USERS` FOREIGN KEY (`idUSERS`) REFERENCES `users` (`idUSERS`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messages` (
+  `idMESSAGES` int NOT NULL AUTO_INCREMENT,
+  `idUSERS` int NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  `attachment` varchar(120) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message_parent` int DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`idMESSAGES`),
+  UNIQUE KEY `idMESSAGES_UNIQUE` (`idMESSAGES`),
+  KEY `fk_MESSAGES_USERS_idx` (`idUSERS`),
+  KEY `fk_MESSAGES_MESSAGES_idx` (`message_parent`),
+  CONSTRAINT `fk_MESSAGES_MESSAGES` FOREIGN KEY (`message_parent`) REFERENCES `messages` (`idMESSAGES`) ON DELETE CASCADE,
+  CONSTRAINT `fk_MESSAGES_USERS` FOREIGN KEY (`idUSERS`) REFERENCES `users` (`idUSERS`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1057 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
@@ -35,7 +76,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `idUSERS_UNIQUE` (`idUSERS`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=155 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=242 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -47,4 +88,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-25 14:19:13
+-- Dump completed on 2020-07-10 11:46:25
